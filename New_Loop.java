4e6e7025bc8efd9031a082c64db987084ba9e2e7
ramji123456 <t.ramji994@gmail.com>
2022-06-20 08:47:07 +0530

    scrrenshot

---
+++ New_Loop.java
@@ -0,0 +1,16 @@
+package prime;
+
+public class New_Loop {
+
+	public static void main(String[] args) {
+		int i,j=0;
+		for(i=5;i>=1;i--) {
+			for(j=5;j==5;i--) {
+			
+			System.out.print("*");
+ 
+	}
+			System.out.println("*");
+	}
+}
+}
